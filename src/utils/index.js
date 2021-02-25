0bb91f75c4d7a3bb60d274b915bc1e5a0065370a
Paul Taiwo <ayopaulot@gmail.com>
2021-02-25 20:21:24 +0100

    add auth utils function

---
+++ src/utils/index.js
@@ -0,0 +1,11 @@
+const CRED = "credentials";
+
+export const saveUserCredentials = (credentials) => {
+  localStorage.setItem(CRED, JSON.stringify(credentials));
+};
+
+export const getUserCredentials = () => {
+  const credentials = localStorage.getItem(CRED);
+
+  return JSON.parse(credentials);
+};
