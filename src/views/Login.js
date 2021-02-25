5a3cb68e11aede90c399c4a657560dfae01c2ad6
Paul Taiwo <ayopaulot@gmail.com>
2021-02-25 20:24:10 +0100

    integrate login endpoint

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 
 // reactstrap components
@@ -14,75 +13,105 @@ import {
   InputGroup,
   Row,
   Col,
+  Spinner,
 } from "reactstrap";
+import { useFormik } from "formik";
+import { useAlert } from 'react-alert';
+import axiosClient from "services/axiosClient";
+import { saveUserCredentials } from "utils";
+import { useHistory } from "react-router-dom";
 
 const Login = () => {
+  const alert = useAlert();
+  const history = useHistory();
+  const loginForm = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: (values, {setSubmitting}) => {
+      axiosClient
+        .post("/admin/login", {...values})
+        .then((response) => {
+          saveUserCredentials(response.data);
+          history.push('/admin')
+        })
+        .catch((error) => {
+          setSubmitting(false);
+          const errMessage = error.response.data.msg || error.message;
+          alert.error(errMessage);
+        });
+    },
+  });
+
   return (
     <>
-      <Col lg="5" md="7">
-        <Card className="bg-secondary shadow border-0">
-
-          <CardBody className="px-lg-5 py-lg-5">
-            <div className="text-center text-muted mb-4">
+      <Col lg='5' md='7'>
+        <Card className='bg-secondary shadow border-0'>
+          <CardBody className='px-lg-5 py-lg-5'>
+            <div className='text-center text-muted mb-4'>
               <small>Sign in with credentials</small>
             </div>
-            <Form role="form">
-              <FormGroup className="mb-3">
-                <InputGroup className="input-group-alternative">
-                  <InputGroupAddon addonType="prepend">
+            <Form role='form' onSubmit={loginForm.handleSubmit}>
+              <FormGroup className='mb-3'>
+                <InputGroup className='input-group-alternative'>
+                  <InputGroupAddon addonType='prepend'>
                     <InputGroupText>
-                      <i className="ni ni-email-83" />
+                      <i className='ni ni-email-83' />
                     </InputGroupText>
                   </InputGroupAddon>
                   <Input
-                    placeholder="Email"
-                    type="email"
-                    autoComplete="new-email"
+                    placeholder='Email'
+                    type='email'
+                    id='email'
+                    value={loginForm.values.email}
+                    onChange={loginForm.handleChange}
+                    autoComplete='new-email'
                   />
                 </InputGroup>
               </FormGroup>
               <FormGroup>
-                <InputGroup className="input-group-alternative">
-                  <InputGroupAddon addonType="prepend">
+                <InputGroup className='input-group-alternative'>
+                  <InputGroupAddon addonType='prepend'>
                     <InputGroupText>
-                      <i className="ni ni-lock-circle-open" />
+                      <i className='ni ni-lock-circle-open' />
                     </InputGroupText>
                   </InputGroupAddon>
                   <Input
-                    placeholder="Password"
-                    type="password"
-                    autoComplete="new-password"
+                    placeholder='Password'
+                    type='password'
+                    id='password'
+                    onChange={loginForm.handleChange}
+                    autoComplete='new-password'
                   />
                 </InputGroup>
               </FormGroup>
-              <div className="custom-control custom-control-alternative custom-checkbox">
+              {/* <div className='custom-control custom-control-alternative custom-checkbox'>
                 <input
-                  className="custom-control-input"
-                  id=" customCheckLogin"
-                  type="checkbox"
+                  className='custom-control-input'
+                  id=' customCheckLogin'
+                  type='checkbox'
                 />
-                <label
-                  className="custom-control-label"
-                  htmlFor=" customCheckLogin"
-                >
-                  <span className="text-muted">Remember me</span>
+                <label className='custom-control-label' htmlFor=' customCheckLogin'>
+                  <span className='text-muted'>Remember me</span>
                 </label>
-              </div>
-              <div className="text-center">
-                <Button className="my-4" color="primary" type="button">
+              </div> */}
+              <div className='text-center'>
+                <Button
+                  disabled={loginForm.isSubmitting}
+                  className='my-4'
+                  color='primary'
+                  type='submit'>
                   Sign in
+                  {loginForm.isSubmitting && <Spinner size='sm' className='ml-1' />}
                 </Button>
               </div>
             </Form>
           </CardBody>
         </Card>
-        <Row className="mt-3">
-          <Col xs="6">
-            <a
-              className="text-light"
-              href="#pablo"
-              onClick={(e) => e.preventDefault()}
-            >
+        <Row className='mt-3'>
+          <Col xs='6'>
+            <a className='text-light' href='#pablo' onClick={(e) => e.preventDefault()}>
               <small>Forgot password?</small>
             </a>
           </Col>
