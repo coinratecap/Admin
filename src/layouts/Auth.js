d084ec11d15777432a9cfd8d0c5e07e5c6e59eb9
Paul Taiwo <ayopaulot@gmail.com>
2021-02-25 18:54:07 +0100

    restructure Auth

--- src/layouts/Auth.js
+++ src/layouts/Auth.js
@@ -7,7 +7,8 @@ import { Container, Row } from "reactstrap";
 // import AuthNavbar from "components/Navbars/AuthNavbar.js";
 // import AuthFooter from "components/Footers/AuthFooter.js";
 
-import routes from "routes.js";
+// import routes from "routes.js";
+import Login from "views/Login";
 
 const Auth = (props) => {
   const mainContent = React.useRef(null);
@@ -25,50 +26,43 @@ const Auth = (props) => {
     mainContent.current.scrollTop = 0;
   }, [location]);
 
-  const getRoutes = (routes) => {
-    return routes.map((prop, key) => {
-      if (prop.layout === "/auth") {
-        return (
-          <Route
-            path={prop.layout + prop.path}
-            component={prop.component}
-            key={key}
-          />
-        );
-      } else {
-        return null;
-      }
-    });
-  };
+  // const getRoutes = (routes) => {
+  //   return routes.map((prop, key) => {
+  //     if (prop.layout === "/auth") {
+  //       return (
+  //         <Route path={prop.layout + prop.path} component={prop.component} key={key} />
+  //       );
+  //     } else {
+  //       return null;
+  //     }
+  //   });
+  // };
 
   return (
     <>
-      <div className="main-content" ref={mainContent}>
+      <div className='main-content' ref={mainContent}>
         {/* <AuthNavbar /> */}
-        <div className="header bg-gradient-info py-7 py-lg-8">
-          <div className="separator separator-bottom separator-skew zindex-100">
+        <div className='header bg-gradient-info py-7 py-lg-8'>
+          <div className='separator separator-bottom separator-skew zindex-100'>
             <svg
-              xmlns="http://www.w3.org/2000/svg"
-              preserveAspectRatio="none"
-              version="1.1"
-              viewBox="0 0 2560 100"
-              x="0"
-              y="0"
-            >
-              <polygon
-                className="fill-default"
-                points="2560 0 2560 100 0 100"
-              />
+              xmlns='http://www.w3.org/2000/svg'
+              preserveAspectRatio='none'
+              version='1.1'
+              viewBox='0 0 2560 100'
+              x='0'
+              y='0'>
+              <polygon className='fill-default' points='2560 0 2560 100 0 100' />
             </svg>
           </div>
         </div>
 
         {/* Page content */}
-        <Container className="mt--8 pb-5">
-          <Row className="justify-content-center">
+        <Container className='mt--8 pb-5'>
+          <Row className='justify-content-center'>
             <Switch>
-              {getRoutes(routes)}
-              <Redirect from="*" to="/auth/login" />
+              {/* {getRoutes(routes)} */}
+              <Route path='/auth/login' component={Login} />
+              <Redirect from='*' to='/auth/login' />
             </Switch>
           </Row>
         </Container>
