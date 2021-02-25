73802cbf3f9e0004f1da0588da0c50ad20fb49d1
Paul Taiwo <ayopaulot@gmail.com>
2021-02-25 20:22:56 +0100

    add Alert Provider

--- src/index.js
+++ src/index.js
@@ -20,25 +20,37 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
-
+import { transitions, positions, Provider as AlertProvider } from "react-alert";
 import "assets/plugins/nucleo/css/nucleo.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "assets/scss/argon-dashboard-react.scss";
 import store from "redux/store";
 
+import AlertTemplate from "components/AlertTemplate";
 import AdminLayout from "layouts/Admin.js";
 import AuthLayout from "layouts/Auth.js";
+
+const options = {
+  // you can also just use 'bottom center'
+  position: positions.TOP_CENTER,
+  timeout: 5000,
+  offset: "30px",
+  // you can also just use 'scale'
+  transition: transitions.SCALE,
+};
 
 ReactDOM.render(
   <Provider store={store().store}>
     <PersistGate loading={null} persistor={store().persistor}>
-      <BrowserRouter>
-        <Switch>
-          <Route path='/admin' render={(props) => <AdminLayout {...props} />} />
-          <Route path='/auth' render={(props) => <AuthLayout {...props} />} />
-          <Redirect from='/' to='/admin/index' />
-        </Switch>
-      </BrowserRouter>
+      <AlertProvider template={AlertTemplate} {...options}>
+        <BrowserRouter>
+          <Switch>
+            <Route path='/admin' render={(props) => <AdminLayout {...props} />} />
+            <Route path='/auth' render={(props) => <AuthLayout {...props} />} />
+            <Redirect from='/' to='/admin/index' />
+          </Switch>
+        </BrowserRouter>
+      </AlertProvider>
     </PersistGate>
   </Provider>,
   document.getElementById("root")
